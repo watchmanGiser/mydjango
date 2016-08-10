47dd9ae25a176b2835f8375d3f4d3bb319065d9c
zhuzhongpeng <zhuzhongpeng@navinfo.com>
2016-08-10 23:58:41 +0800

    my simple blog 2 commit

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog.views import hello
 
 
 
@@ -9,5 +10,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/',hello),
 
 ]
