251e25dcdc69227a0d7a78161f3354a15010b524
zhuzhongpeng <zhuzhongpeng@navinfo.com>
2016-08-11 17:17:05 +0800

    测试显示中文

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
@@ -5,5 +6,5 @@ def hello(self):
     return HttpResponse("hello")
 
 def fuc_welc(self):
-    html_str = "<html><body><h1>Welcome to here,building!</h1></body></html>"
+    html_str = "<html><body><h1>Welcome to here,building!这是一个django项目</h1></body></html>"
     return  HttpResponse(html_str)
